63a5888ad6a39dc12f2ac3a258d45cbaaa45eeef
antonina palchikova <aapalchikova@avito.ru>
2019-03-25 11:59:48 +0300

    added steps 2-5

--- omd.py
+++ omd.py
@@ -1,20 +1,50 @@
 #  coding: utf-8
 # toneypal - Antonina Palchikova
 
-def step1():
+option = ''
+options = {'да': True, 'нет': False}
+
+
+def step1(option):
     print(
         'Утка-маляр     решила выпить зайти в бар.  '
         'Взять ей зонтик?   '
     )
-    option = ''
-    options = {'да': True, 'нет': False}
+
     while option not in options:
-        print ('Выберете: {}/{}'.format(*options))
+        print('Выберете: {}/{}'.format(*options))
         option = input()
 
-    if options[option]:
+        if options[option]:
+            step4_rain()
+        else:
+            step3_without_umbrella()
+
+
+def step2_umbrella():
+    print('Утка идет c зонтиком')
+
+
+def step3_without_umbrella():
+    print('Утка идет без зонтика')
+
+
+def step4_rain():
+    print('На улице идет дождь?')
+    rain = input()
+    if options[rain]:
+        step2_umbrella()
+    else:
+        step5_cloudy()
+
+
+def step5_cloudy():
+    cloudy = input('На улице облачно?')
+    if options[cloudy]:
         return step2_umbrella()
-    return step2_umbrella()
+    else:
+        return step3_without_umbrella()
+
 
 if __name__ == '__main__':
-    step1()
+    step1(option)
